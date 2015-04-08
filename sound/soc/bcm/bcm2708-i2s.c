5ab757fc57982cefd57e51e0a3cd08e50b91ee96
Marc CAPDEVILLE <m.capdeville@no-log.org>
2015-04-08 12:05:20 +0200

    bcm2708-i2s : Ajout des flags MMAP

--- sound/soc/bcm/bcm2708-i2s.c
+++ sound/soc/bcm/bcm2708-i2s.c
@@ -876,7 +876,9 @@ static const struct snd_soc_component_driver bcm2708_i2s_component = {
 
 static const struct snd_pcm_hardware bcm2708_pcm_hardware = {
 	.info			= SNDRV_PCM_INFO_INTERLEAVED |
-				  SNDRV_PCM_INFO_JOINT_DUPLEX,
+				  SNDRV_PCM_INFO_JOINT_DUPLEX |
+				  SNDRV_PCM_INFO_MMAP |
+				  SNDRV_PCM_INFO_MMAP_VALID,
 	.formats		= SNDRV_PCM_FMTBIT_S16_LE |
 				  SNDRV_PCM_FMTBIT_S24_LE |
 				  SNDRV_PCM_FMTBIT_S32_LE,
